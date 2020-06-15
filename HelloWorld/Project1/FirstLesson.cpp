c2462fd33a663009de841d2bf5d425a1ac46ed53
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-14 20:24:41 -0700

    Update FirstLesson.cpp

--- HelloWorld/Project1/FirstLesson.cpp
+++ HelloWorld/Project1/FirstLesson.cpp
@@ -1,3 +1,21 @@
 /*The first lesson*/
 
-/**/
+/* 
+			Basic print
+
+cout: console out
+
+endl: end line
+
+basic print function:
+
+cout<< "Hello my name is Nhan"<<endl
+
+-------------------------------------------
+			Variables
+
+
+
+
+
+*/
