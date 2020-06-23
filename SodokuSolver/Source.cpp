0b266d865db677fa87756bc9e02cf16c70c1700c
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-06-22 21:05:20 -0700

    Update Source.cpp

--- SodokuSolver/Source.cpp
+++ SodokuSolver/Source.cpp
@@ -7,70 +7,58 @@ using namespace std;
 
 int gameboard[9][9];
 
-int limit = 9;
+int const limit = 9;
 
-bool Isrow(int a, int b, int c) {
+bool Isrow(int a, int b, int c){
 
+	for (int x = 0; x < limit; x++){
 
-	for (int x = 0; x < limit; x++) {
-
-		if (gameboard[a][x] == c) {
-
-			return 0;
-
-		}
-
-		return 1;
-
-	}
-
-}
-
-bool Iscolumns(int a, int b, int c) {
-
-	int limit = 9;
-
-	for (int x = 0; x < limit; x++) {
-
-		if (gameboard[x][b] == c) {
+		if (gameboard[a][x] == c){
 
 			return 0;
-
-		}
-
-		return 1;
-
-	}
-
-}
-
-bool Issquare(int a, int b, int c) {
-
-	int row = ceil((a + 1) / 3.);
-
-	int column = ceil((b + 1) / 3.);
-
-	for (int x = (row - 1) * 3; x < (row - 1) * 3 + 3; x++) {
-
-		for (int y = (column - 1) * 3; y < (column - 1) * 3 + 3; y++) {
-
-			if (gameboard[x][y] == c) {
-
-				return 0;
-
-			}
 
 		}
 
 	}
 
 	return 1;
+}
 
+bool Iscolumns(int a, int b, int c){
+
+	for (int y = 0; y < 9; y++){
+
+		if (gameboard[y][b] == c)
+
+			return 0;
+
+	}
+
+	return 1;
+
+}
+bool Issquare(int a, int b, int c)
+{
+	int row = ceil((a + 1) / 3.);
+
+	int column = ceil((b + 1) / 3.);
+
+	for (int x = (row - 1) * 3; x < ((row - 1) * 3 + 3); x++)
+
+		for (int y = (column - 1) * 3; y < ((column - 1) * 3 + 3); y++){
+
+			if (gameboard[x][y] == c)
+
+				return 0;
+
+		}
+
+	return 1;
 }
 
 void solving() {
 
-	int unkown = 0, Row, Column, tempval;
+	int unknow = 0, Row, Column, tempval;
 
 	bool b = 1;
 
@@ -78,7 +66,7 @@ void solving() {
 
 	cout << "Please enter your file path" << endl;
 
-	getline(cin, inputFile);
+	cin >> inputFile;
 
 	ifstream input(inputFile);
 
@@ -94,18 +82,17 @@ void solving() {
 
 			input >> gameboard[i][j];
 
-			if (!gameboard[i][j]) {
+			if (gameboard[i][j]==0) 
 
-				unkown++;
+				unknow++;
 
-				cout << "Unknow: " << unkown<<endl;
-			}
+			
 		}
 	}
 
 	input.close();
 
-	while (unkown && b) {
+	while (unknow && b) {
 
 		b = 0;
 
@@ -113,23 +100,21 @@ void solving() {
 
 			for (int j = 0; j < limit; j++) {
 
-				if (gameboard[i][j]) {
+				if (gameboard[i][j]!=0) 
 
 					continue;
 
-				}
-
 				tempval = 0;
 
-				for (int x = 1; x <= limit; x++) {
+				for (int x = 1; x < limit+1; x++) {
 
 					if (Isrow(i, j, x) && Iscolumns(i, j, x) && Issquare(i, j, x)) {
 
-						if (!tempval) {
+						if (!tempval) 
 
 							tempval = x;
 
-						}
+						
 						else {
 
 							tempval = 0;
@@ -141,13 +126,14 @@ void solving() {
 					}
 				}
 
-				if (tempval) {
+				if (tempval!=0)
+				{
 
 					gameboard[i][j] = tempval;
 
 					b = 1;
 
-					unkown--;
+					unknow--;
 
 				}
 
@@ -165,17 +151,19 @@ void solving() {
 
 		cout << "Sodoku is solved: \n\n\n" << endl;
 
-		for (int i = 0; i < limit; i++) {
+		
 
-			for (int j = 0; j < limit; j++) {
+	}
 
-				cout << gameboard[i][j] << " ";
+	for (int i = 0; i < limit; i++) {
 
+		for (int j = 0; j < limit; j++) {
 
-			}
-			cout << "\n\n\n";
+			cout << gameboard[i][j] << " ";
+
 
 		}
+		cout << "\n\n\n";
 
 	}
 
