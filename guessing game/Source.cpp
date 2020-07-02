3860f575e2bef98ddebca122435576d7eca97b19
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-01 20:53:14 -0700

    Update Source.cpp

--- guessing game/Source.cpp
+++ guessing game/Source.cpp
@@ -16,10 +16,15 @@ int main() {
 	int userInput=-1;
 	
 	int guessLimit = 0;
+	
 	while (answer != userInput && guessLimit<3) {
+		
 		cout << "You have " << 3 - guessLimit<<" guesses"<< endl;
+		
 		cout << "Please enter your guessing number: ";
+		
 		cin >> userInput;
+		
 		guessLimit++;
 	}
 	if (guessLimit == 3) {
