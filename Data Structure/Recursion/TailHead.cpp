12b4e507df559b50b9318f6ceee924e3da5a11b9
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-23 10:39:07 -0700

    Update TailHead.cpp

--- Data Structure/Recursion/TailHead.cpp
+++ Data Structure/Recursion/TailHead.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 using namespace std;
 
-void fun(int n) {
+int fun(int n) {
+	static int x = 0;// Static variable is created in code section. Different from heap in Java.
 	if (n > 0) {
+		x++;
 		cout << n << endl;
 		fun(n - 1);
 	}
+	return fun(x - 1) + x;
 }
 void main() {
 	int x = 3;
