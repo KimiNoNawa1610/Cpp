bb822bb2b0091101cea016cd4251be4258323a15
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-07-27 20:43:29 -0700

    update array

--- Data Structure/AbstractArrays/Array.h
+++ Data Structure/AbstractArrays/Array.h
@@ -1,1 +1,53 @@
 #pragma once
+#include<iostream>
+#ifndef ARRAYDB_ARRAY_H
+#define ARRAYDB_ARRAY_H
+
+using namespace std;
+
+class Array {
+
+private:
+	int* A;
+
+	int length, size;
+
+public:
+	Array(int si);
+
+	~Array();
+
+	int getLength();
+
+	void insert(int ii, int index);
+
+	void append(int ii);
+
+	void display();
+
+	int drop(int index);
+
+	int get(int index);
+
+	void set(int number, int index);
+
+	int max();
+
+	int min();
+
+	int sum();
+
+	int avg();
+
+	bool isSorted();
+
+	void arrage();
+
+	Array merge(Array S);
+
+	int *getArray();
+		
+
+};
+
+#endif // !ARRAYDB_ARRAY_H
--- Data Structure/AbstractArrays/Chanllenges.cpp
+++ Data Structure/AbstractArrays/Chanllenges.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include"Array.h"
 using namespace std;
 
 /* Find missing element in sorted Array*/
 
 /*Single element*/
+int findsingle(Array s) {
+	int x = s.sum();
+	int total, low, height;
+	low = s.getArray()[s.getLength()];
+	height = low + 1;
+	total = (low * height) / 2;
+	return total - x;
+}
+
+/*Multiple elements*/
