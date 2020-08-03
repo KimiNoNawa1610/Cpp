260a715b4d6dcaa273679f41a4c9f1068f2ee162
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-08-03 13:15:37 -0700

    update infix and post fix

--- Data Structure/Stack/ArrayStack.cpp
+++ Data Structure/Stack/ArrayStack.cpp
@@ -5,13 +5,14 @@ class Stack {
 
 private:
 
-	int size, top, * S;
+	int size, top;
+	char *S;
 
 public:
 
 	Stack(int size) {
 		this->size = size;
-		S =new int[size];
+		S =new char[size];
 		top = -1;
 	}
 
@@ -27,7 +28,7 @@ public:
 		return a;
 	}
 
-	void push(int x) {
+	void push(char x) {
 		if (top == size - 1) {
 			cout << "Stack Overflow";
 		}
@@ -37,19 +38,19 @@ public:
 		}
 	}
 
-	int pop() {
-		int x = -1;
+	char pop() {
+		char x=NULL;
 		if (top == -1) {
 			cout << "Empty Stack";
 		}
 		else {
-			top--;
 			x = S[top];
+			top--;
 		}
 		return x;
 	}
 
-	int peek(int index) {
+	char peek(int index) {
 		if (top - index + 1 < 0) {
 			cout << "Invalid index";
 		}
@@ -72,11 +73,91 @@ public:
 		return false;
 	}
 
-	int Stacktop() {
+	char Stacktop() {
 		if (!isEmpty()) {
 			return S[top];
 		}
 		return -1;
 	}
+
+	void settop(int num) {
+		top = num;
+	}
 };
+
+bool ParenthesisMatching(const char* characters) {
+	Stack st = Stack(sizeof(characters));
+	st.settop(-1);
+	for (int i = 0; characters[i] != '\0'; i++) {
+		if (characters[i] == '('||characters[i]=='['|| characters[i] == '{') {
+			st.push(characters[i]);
+		}
+		else if (characters[i] == ')'|| characters[i] == ']'|| characters[i] == '}') {
+			if (st.isEmpty()) {
+				return false;
+			}
+			st.pop();
+		}
+	}
+	if (st.isEmpty()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool isOperrand(char x) {
+	if (x == '+' || x == '*' || x == '/' || x == '-') {
+		return false;
+	}
+	return true;
+}
+
+int value(const char x) {
+	if (x == '+' || x == '-') {
+		return 1;
+	}
+	else if (x == '*' || x == '/') {
+		return 2;
+	}
+	return 0;
+}
+
+char* ConvertInfixtoPostfix(const char* characters) {
+	Stack st = Stack(sizeof(characters));
+	char* postfix = new char[sizeof(characters) + 2];
+	int i,j;
+	i = j = 0;
+	while (characters[i] != '\0') {
+		if (isOperrand(characters[i])) {
+			postfix[j++] = characters[i++];
+		}
+		else {
+			if (value(st.Stacktop()) < value(characters[i])) {
+				st.push(characters[i++]);
+			}
+			else {
+				postfix[j++] = st.pop();
+			}
+		}
+	}
+	while (!st.isEmpty()) {
+		postfix[j++] = st.pop();
+	}
+	postfix[j] = '\0';
+	return postfix;
+}
+
+int evaluation(const char* postfix) {
+
+}
+
+int main() {
+	const char *example1="((a+b)*(c+d))";
+	cout << ParenthesisMatching(example1)<<endl;
+	const char* example2 = "a+b*c-d/e";
+	cout << ConvertInfixtoPostfix(example2);
+	return 0;
+}
 
