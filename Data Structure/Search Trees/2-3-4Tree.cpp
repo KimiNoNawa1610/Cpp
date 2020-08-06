fd2f2af63ca7491c70af913fcc39d07e3e2a0a1c
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-08-06 12:21:43 -0700

    Create 2-3-4Tree.cpp

---
+++ Data Structure/Search Trees/2-3-4Tree.cpp
@@ -0,0 +1,19 @@
+/*
+/*
+Multiway high balanced search tree with the degree 4
+
+ Rule:
+
+ All leaf nodes are at the same level
+
+ Everynode mus have minimum of 2 children and maximum 4 children
+
+ key inside a node must be sorted
+
+ cannot have duplicate element
+
+ In the root, there are k1 and k2 element (k2>k1). Every element in the lefthand side must be less than k1. The element in the middle must be greater than k1 and less than k2.
+ All element in the right handside must be greater than k2
+
+*/
+
