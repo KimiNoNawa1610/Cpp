dfa4141504c1d5bcbfea93a709f14071e58e5fac
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-08-06 12:41:05 -0700

    update tree

---
+++ Data Structure/Search Trees/Red-BlackTree.cpp
@@ -0,0 +1,17 @@
+/*
+Height balanced binary search tree, similar to 2-3-4 tree
+
+EveryNode is either Red or Black
+
+Root of a tree is Black
+
+Null is also black
+
+Number of Blacks on the path from root to leaf are the same
+
+No 2 consecutive red node
+
+New inserted node are red
+
+Height is min log(n) and max is 2log(n)
+*/
