6b4bd6c58d05c6dbb8b40d06e7b4ded08a13fbcb
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-08-10 20:50:00 -0700

    update sorting

---
+++ Sorting/ShellSort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void ShellSort(int A[], int n) {
+    for (int gap = n / 2; gap >= 1; gap /= 2) {
+        for (int j = gap; j < n; j++) {
+            int temp = A[j];
+            int i = j - gap;
+            while (i >= 0 && A[i] > temp) {
+                A[i + gap] = A[i];
+                i = i - gap;
+            }
+            A[i + gap] = temp;
+        }
+    }
+}
